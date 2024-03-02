b78f147a0e41ea2dd3c1994e1ba6ff7dc6371fe2
root <root@ip-172-31-7-12.ap-south-1.compute.internal>
2024-03-02 07:31:30 +0000

    This is my second commit

--- file.java
+++ file.java
@@ -24,14 +24,4 @@ System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");
 System.out.println();  
 }  
 }  
-//driver code  
-public static void main(String args[])  
-{  
-Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the value of n: ");  
-int n = sc.nextInt();  
-System.out.println();  
-//function calling  
-printPattern(n);  
-}  
-}  
+This is my second commit!  
